01164063b2edb8b0714ba69d69bbd3cd15055ee6
Lucas Couto <loc.unb@gmail.com>
2022-05-11 15:30:15 -0300

    Passa a função _responder como argumento

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -4,14 +4,22 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() onSelecao;
 
-  const Resposta(this.texto, {Key? key}) : super(key: key);
+  const Resposta(this.texto, this.onSelecao, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      child: Text(texto),
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(primary: Colors.blue),
+        onPressed: onSelecao,
+        child: Text(
+          texto,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
